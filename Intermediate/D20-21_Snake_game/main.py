212edff0b15224c33ffe2b4b4a1ba6b132e3ee69
Amogh-Borikar <amoghborikar@gmail.com>
2024-05-09 20:44:41 -0700

    extension changes

---
+++ Intermediate/D20-21_Snake_game/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game!")
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
+    segment_1 = Turtle(shape="square")
+    segment_1.color("white")
+    segment_1.goto(position)
+
+screen.exitonclick()
