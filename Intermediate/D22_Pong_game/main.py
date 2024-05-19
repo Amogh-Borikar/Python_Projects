22a376ccc868c0373a5bdb7b475d90991c4e09fc
Amogh-Borikar <amoghborikar@gmail.com>
2024-05-18 18:41:50 -0700

    extension changes

--- Intermediate/D22_Pong_game/main.py
+++ Intermediate/D22_Pong_game/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 my_screen = Screen()
@@ -12,6 +13,22 @@ my_screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
+draw_screen = Turtle()
+
+draw_screen.hideturtle()
+draw_screen.penup()
+draw_screen.color("white")
+draw_screen.goto(400, 300)
+draw_screen.pendown()
+draw_screen.goto(400, -300)
+draw_screen.goto(-400, -300)
+draw_screen.goto(-400, 300)
+draw_screen.goto(400, 300)
+draw_screen.penup()
+draw_screen.goto(0, 300)
+draw_screen.pendown()
+draw_screen.goto(0, -300)
 
 my_screen.listen()
 my_screen.onkey(r_paddle.go_up, "Up")
@@ -22,11 +39,22 @@ my_screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 while game_is_on:
     ball.move()
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     my_screen.update()
 
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 my_screen.exitonclick()
 
