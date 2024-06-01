744f8cbd81aeeec7e1b09f32a7975fda6c677544
Amogh-Borikar <amoghborikar@gmail.com>
2024-06-01 15:15:45 -0700

    D30_Error_handelling

--- Intermediate/D30_Error_handelling/Password_manager/main.py
+++ Intermediate/D30_Error_handelling/Password_manager/main.py
@@ -38,11 +38,43 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("Intermediate/D30_Error_handelling/Password_manager/data.json", "w") as data_file:
-            json.dump(new_data, data_file, indent=4)
+        try:
+            with open("Intermediate/D30_Error_handelling/Password_manager/data.json", "r") as data_file:
+                # load json file
+                data = json.load(data_file)
+        
+        except:
+            with open("Intermediate/D30_Error_handelling/Password_manager/data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        
+        else:
+            # update the file with new data
+            data.update(new_data)
+
+            with open("Intermediate/D30_Error_handelling/Password_manager/data.json", "w") as data_file:
+                # Save data to file
+                json.dump(data, data_file, indent=4)
+
+        finally: 
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("Intermediate/D30_Error_handelling/Password_manager/data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found!")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"{website}\nEmail: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details exist for {website}.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -64,12 +96,12 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=39)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
+email_entry.insert(0, "xyz@xyz.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
@@ -78,5 +110,7 @@ generate_password_button = Button(text="Generate Password", command=generate_pas
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
